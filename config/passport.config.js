d171a4cb52e3c1594b1806ca6285162284530fb5
otaviokochi <otaviokochi@gmail.com>
2021-03-24 11:29:49 -0300

    removendo comentario

--- config/passport.config.js
+++ config/passport.config.js
@@ -29,33 +29,3 @@ const authenticate = () => passport.authenticate('jwt', { session: false })
 module.exports = {
   authenticate
 }
-
-
-// const localStrategy = require('passport-local').Strategy;
-// const User = require('../model/user.model');
-// const md5 = require('md5');
-
-// module.exports = function(passport) {
-//   passport.use(
-//     new localStrategy({usernameField: 'username', passwordField: 'password'}, (username, password, done) => {
-
-//       User.findByLogin(username, (err, user) => {
-//         if (err)  return done(err); 
-//         if (user.length == 0)  return done(null, false, {message: 'Credenciais erradas!'});
-//         if (md5(password) === user[0].password) return done(null, user[0]);
-//         else return done(null, false, {message: 'Credenciais erradas!'});
-//       })
-//     })
-//   );
-
-//   passport.serializeUser((user, done) => {
-//     return done(null, user.id);
-//   });
-
-//   passport.deserializeUser((id, done) => {
-//     User.findById(id, (err, user) => {
-//       return done(err, user.username);
-//     })
-//   });
-
-// }
